ceebfb7310a86dae609207aff8be1b4059134677
Andrew Sykes <andrewsykes@btinternet.com>
2022-02-03 16:10:48 +0000

    tidy

--- js/app.js
+++ js/app.js
@@ -1,98 +1,56 @@
 /*jslint nomen: true */
 /*global window: false */
 /*global document: false */
-(function() {
+(function () {
 	"use strict";
-    
-    function slice(){
-        return Array.prototype.slice;
-    }
-    
-    function dummy(){}
-    
-    function always(arg){
-        return function(){
-            return arg;
-        };
-    }
-    
-    function dopartial(defer){
-        return function _partial(f){
-            var args = slice().call(arguments, 1);
 
-            if(f.length === args.length){
-                if(defer){
-                    return function(){
-                      return f.apply(null, args);  
-                    };
-                }
-                return f.apply(null, args);
-            }
-        return function(){
-            var _args = slice().call(arguments);
-            return _partial.apply(null, [f].concat(args, _args));
-        };
-    };
-}
+	function dummy() {}
+
+	function dopartial(defer) {
+		var slice = Array.prototype.slice;
+		return function _partial(f) {
+			var args = slice.call(arguments, 1);
+			if (f.length === args.length) {
+				if (defer) {
+					return function () {
+						return f.apply(null, args);
+					};
+				}
+				return f.apply(null, args);
+			}
+			return function () {
+				var _args = slice.call(arguments);
+				return _partial.apply(null, [f].concat(args, _args));
+			};
+		};
+	}
 
 	function defer(fun) {
-		return function(a) {
-			return function() {
+		return function (a) {
+			return function () {
 				return fun(a);
 			};
 		};
 	}
-    
+
 	function curry2(fun) {
-		return function(b) {
-			return function(a) {
+		return function (b) {
+			return function (a) {
 				return fun(a, b);
 			};
 		};
 	}
 
-	function curry22(fun) {
-		return function(b) {
-			return function(a) {
-				return function() {
-					return fun(a, b);
-				};
-			};
-		};
-	}
-
 	function curry3(fun) {
-		return function(c) {
-			return function(b) {
-				return function(a) {
+		return function (c) {
+			return function (b) {
+				return function (a) {
 					return fun(a, b, c);
 				};
 			};
 		};
 	}
-
-	function curry33(fun) {
-		return function(c) {
-			return function(b) {
-				return function(a) {
-					return function() {
-						return fun(a, b, c);
-					};
-				};
-			};
-		};
-	}
-
-	function curryLeft(fun) {
-		return function(a) {
-			return function(b) {
-				return function(c) {
-					return fun(a, b, c);
-				};
-			};
-		};
-	}
-    /*
+	/*
     function isArray(obj) {
         return Array.isArray ? Array.isArray(obj) : Object.prototype.toString.call(obj) === '[object Array]' 
     }
@@ -110,16 +68,16 @@
 		return fns.reduce(compCB, [].slice.call(arguments, 1));
 	}
     */
-    //https://medium.com/@dtipson/creating-an-es6ish-compose-in-javascript-ac580b95104a
+	//https://medium.com/@dtipson/creating-an-es6ish-compose-in-javascript-ac580b95104a
 	//let compose = (...fns) => fns.reduce( (f, g) => (...args) => f(g(...args)))
 	function compose(fns) {
-		return fns.reduce(function(f, g) {
-			return function() {
+		return fns.reduce(function (f, g) {
+			return function () {
 				return f(g.apply(null, arguments));
 			};
 		});
 	}
-    /*
+	/*
 	function add(a, b) {
 		return a + b;
 	}
@@ -131,60 +89,60 @@
 	function div(a, b) {
 		return a / b;
 	}
-  */
+ 
 	function con(arg) {
 		console.log(arg);
 		return arg;
 	}
-  
+     */
 	var main = document.querySelector('main'),
 		him = document.querySelector('.him'),
-        deferpartial = dopartial(true),
-        partial = dopartial(),
-		drill = function(o, p) {
+		deferpartial = dopartial(true),
+		partial = dopartial(),
+		drill = function (o, p) {
 			o = o[p];
 			return o;
 		},
-		best = function(pred, actions) {
-			return actions.reduce(function(champ, contender) {
+		best = function (pred, actions) {
+			return actions.reduce(function (champ, contender) {
 				champ = pred() ? champ : contender;
 				return champ;
 			});
 		},
-		invoke = function(f, arg) {
+		invoke = function (f, arg) {
 			return f(arg);
 		},
-		doScroll = function() {
+		doScroll = function () {
 			document.body.scrollTop = 0; // For Safari
 			document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 		},
-		deferScroll = function() {
+		deferScroll = function () {
 			window.setTimeout(doScroll, 444);
 		},
-		notExternal = function(tgt) {
+		notExternal = function (tgt) {
 			return tgt.href.indexOf(window.location.href) >= 0;
 		},
-		equals = function(a, b) {
+		equals = function (a, b) {
 			return a === b;
 		},
-		getProp = function(o, p) {
+		getProp = function (o, p) {
 			return p ? o[p] : o;
 		},
-		invokeProp = function(o, p, v) {
+		invokeProp = function (o, p, v) {
 			return o[p](v);
 		},
-		setProp = function(o, p, v) {
-            o[p] = v;
+		setProp = function (o, p, v) {
+			o[p] = v;
 		},
-		doURL = function(src) {
+		doURL = function (src) {
 			return "url(" + src + ")";
 		},
-		each = curry33(invokeProp)(invoke)('forEach'),
-		prevent = curry33(invokeProp)(null)('preventDefault'),
+		foreach = curry3(invokeProp)(invoke)('forEach'),
+		prevent = curry3(invokeProp)(null)('preventDefault'),
 		doNull = curry3(setProp)(null)('backgroundImage'),
 		setPropDefer = partial(setProp),
-		getTargetPicStyle = curry22(getProp)('style')(him),
-		getDataSet = curry22(getProp)('dataset')(him),
+		getTargetPicStyle = defer(curry2(getProp)('style'))(him),
+		getDataSet = defer(curry2(getProp)('dataset'))(him),
 		doEquals = curry3(equals)(null),
 		getTarget = curry2(getProp)('target'),
 		isTargetPic = doEquals(him),
@@ -193,7 +151,7 @@
 		isLocal = compose([notExternal, getTarget]),
 		reSetPic = compose([doNull, getTargetPicStyle]),
 		getCurrent = deferpartial(invokeProp, [him, 'dataset', 'current'], 'reduce', drill),
-		getData = function(str) {
+		getData = function (str) {
 			if (!str) {
 				return '';
 			}
@@ -205,24 +163,23 @@
 		doDataSet = compose([curry2(invoke)('current'), setPropDefer, getDataSet])(),
 		getHREF = curry3(invokeProp)('href')('getAttribute'),
 		deferURL = compose([doURL, getHREF, getTarget]),
-		deferType = compose([ getData, getHREF, getTarget]),
+		deferType = compose([getData, getHREF, getTarget]),
 		setPic = compose([doBg, deferURL]),
 		doDataRESET = defer(doDataSet)(''),
-        validatePic = compose([isTargetPic, getTarget]),
-        reset_actions = [compose([reSetPic, getTarget]), compose([doDataRESET, getTarget])];    
-	main.addEventListener('click', function(e) {
-		var validate = curry33(invokeProp)(curry2(invoke)(e))('every')([isLink, isLocal]),
-            resetWhen = deferpartial(invokeProp, reset_actions, 'map', curry2(invoke)(e)),
-            resetPic = best(defer(validatePic)(e), [resetWhen, dummy]),           
-            preventer = compose([invoke, deferpartial(best, validate, [prevent(e), dummy]) ]),
-			enter = each([defer(setPic)(e), defer(doDataSet)(deferType(e))]),
-			restore = each([reSetPic, doDataRESET]),
+		validatePic = compose([isTargetPic, getTarget]),
+		reset_actions = [compose([reSetPic, getTarget]), compose([doDataRESET, getTarget])];
+	main.addEventListener('click', function (e) {
+		var validate = defer(curry3(invokeProp)(curry2(invoke)(e))('every'))([isLink, isLocal]),
+			resetWhen = deferpartial(invokeProp, reset_actions, 'map', curry2(invoke)(e)),
+			resetPic = best(defer(validatePic)(e), [resetWhen, dummy]),
+			preventer = compose([invoke, deferpartial(best, validate, [defer(prevent)(e), dummy])]),
+			enter = defer(foreach)([defer(setPic)(e), defer(doDataSet)(deferType(e))]),
+			restore = defer(foreach)([reSetPic, doDataRESET]),
 			match = deferpartial(equals, getCurrent(), deferType(e)),
-            thenInvoke = compose([invoke, deferpartial(best, match, [restore, enter])]),
-            doSetPic = compose([invoke, deferpartial(best, validate, [each([thenInvoke, deferScroll]), dummy])]);
-        con(getCurrent())
+			thenInvoke = compose([invoke, deferpartial(best, match, [restore, enter])]),
+			doSetPic = compose([invoke, deferpartial(best, validate, [defer(foreach)([thenInvoke, deferScroll]), dummy])]);
 		preventer();
-        doSetPic();
-        resetPic();
+		doSetPic();
+		resetPic();
 	});
 }());
